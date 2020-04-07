e7b1c0bba6023f3b3b6b5d152640917716b25312
Christian Shaw <cshaw00048@gmail.com>
2020-04-07 11:17:55 -0500

    updating assets folder

--- assets/app.js
+++ assets/app.js
@@ -1,78 +1,13 @@
-var score = 0;
-var currentQuestion = 0;
-var questions = [ {
-    title: "Question 1",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 0
-},
+var card = $("#quiz-area");
+var countStartNumber = 30;
 
-{   title: "Question 2",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 3
-},
+var questions = [{
+  question: "What is the name of Han Solo's ship?",
+  answers: ["Starship Enterprise", "Battle Star Galactica", "Millenium Falcon", "Star Destroyer"],
+  correctAnswer: "Millenium Falcon",
+  image: "assets/images/millenium.gif"
+}, 
 
-{   title: "Question 3",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 1
-},
 
-{   title: "Question 4",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 2
-},
 
-{   title: "Question 5",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 3
-},
-
-{   title: "Question 6",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 2
-},
-
-{   title: "Question 7",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 0
-},
-
-{   title: "Question 8",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 1
-},
-
-{   title: "Question 9",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 3
-},
-
-{   title: "Question 10",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 2
-}
-];
-
-$document.ready(function(){
-    $('.start a').click(function(e){
-        e.preventDefault();
-        $('.start').hide();
-        $('.quiz').show();
-    });
-
-});
-
-function startQuiz(){
-
-}
-
-function showQuestion(){
-
-}
-
-function checkAnswer() {
-
-}
-
-function showSummary() {
-
-}
+}]
