1284292cde17a66fd3a3f8b93ba34bb58c1367c0
Christian Shaw <cshaw00048@gmail.com>
2020-07-22 00:07:05 -0500

    Trivia Game assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,97 @@
-var score = 0;
-var currentQuestion = 0;
-var questions = [ {
-    title: "Question 1",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 0
+var card = $("#quiz-area");
+var countStartNumber = 30;
+
+var questions = [
+  {
+    question: "What is the name of Han Solo's ship?",
+    answers: ["Starship Enterprise","Battle Star Galactica","Millenium Falcon","Star Destroyer",],
+    correctAnswer: "Millenium Falcon",
+    image: "assets/images/millenium.gif",
+  },
+
+  {
+    question: "In the Star Wars film, what invisible power binds the galaxy together?",
+    answers: ["Atoms","The Force","The Heart Shakra","The God Particle",],
+    correctAnswer: "The Force",
+    image: "assets/images/theForce.gif",
+  },
+
+  {
+    question: "What color is Yoda's lightsaber?",
+    answers: ["Red","Yellow","Blue","Green",],
+    correctAnswer: "Green",
+    image: "assets/images/green.gif",
+  },
+
+  {
+    question: "Who is Darth Tyranus also known as?",
+    answers: ["Senator Palpatine","Senator Valorum","Jar-Jar Binks","Count Dooku",],
+    correctAnswer: "Count Dooku",
+    image: "assets/images/countDooku.gif",
+  },
+  {
+    question: "Which star of Revenge of the Sith is the real-life nephew of Denis Lawson who played Wedge in the first Star Wars Trilogy?",
+    answers: ["Ewan McGregor","Jimmy Smits","Hayden Christensen","Christopher Lee",],
+    correctAnswer: "Ewan McGregor",
+    image: "assets/images/ewanMcGregor.gif",
+  },
+  {
+    question: "What is the battle armor used by Boba Fett in the Star Wars?",
+    answers: ["Mandalorian","Battle Star Galactica","X Wing","Death Star",],
+    correctAnswer: "Mandalorian",
+    image: "assets/images/mandalorian.gif",
+  },
+  {
+    question: "What is the famous weapon used by Jedi Knights in the Star Wars series?",
+    answers: ["Blasters","X-Wing Fighter","Katana","Light Saber",],
+    correctAnswer: "Light Saber",
+    image: "assets/images/lightSaber.gif",
+  },
+  {
+    question: "Which Jedi Knight becomes Darth VAder in Star Wars?",
+    answers: ["Count Dooku","Anakin Skywalker","Obi-Wan Kenobi","Senator Palpatine",],
+    correctAnswer: "Anakin Skywalker",
+    image: "assets/images/anakinSkywalker.gif",
+  },
+  {
+    question: "Who is Luke Skywalker and Princess Leia Organa's mother?",
+    answers: ["Senator Bail Organa","Queen Breha Organa","Padme Amidala","Yoda",],
+    correctAnswer: "Padme Amidala",
+    image: "assets/images/padmeAmidala.gif",
+  },
+  {
+    question: "What were the last words spoken in The Empire Strikes Back?",
+    answers: ["Glad that's over!","May the Force be with you.","Live long and prosper.","I'll be back.",],
+    correctAnswer: "May the Force be with you.",
+    image: "assets/images/mtfbwy.gif",
+  }];
+
+var timer;
+
+var game = {
+  questions: question,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
+
+countdown: function() {
+  this.counter--;
+  $("#counter-number").text(this.counter);
+  if (this.counter === 0) {
+    console.log("TIME IS UP!");
+    this.timeUP();
+  }
 },
 
-{   title: "Question 2",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 3
-},
+loadQuestion: function() {
 
-{   title: "Question 3",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 1
-},
+  timer = setInterval(this.countdown.bind(this), 1000);
 
-{   title: "Question 4",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 2
-},
+  card.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
-{   title: "Question 5",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 3
-},
+  for (i = 0; i < questions[this.currentQuestion].answers.length; i++) {
 
-{   title: "Question 6",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 2
-},
-
-{   title: "Question 7",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 0
-},
-
-{   title: "Question 8",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 1
-},
-
-{   title: "Question 9",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 3
-},
-
-{   title: "Question 10",
-    answers: ['a', 'b', 'c', 'd'],
-    correct: 2
+  }
 }
-];
-
-$document.ready(function(){
-    $('.start a').click(function(e){
-        e.preventDefault();
-        $('.start').hide();
-        $('.quiz').show();
-    });
-
-});
-
-function startQuiz(){
-
-}
-
-function showQuestion(){
-
-}
-
-function checkAnswer() {
-
-}
-
-function showSummary() {
-
-}
+};
